319bc8cdf51ca05d48bab0aa86b729680f1b80d1
Nadja <nadja.strobbe@cern.ch>
2013-11-13 11:33:27 +0100

    Added Vhad cross sections, updated joinfiles.py

--- analyzer/joinfiles.py
+++ analyzer/joinfiles.py
@@ -3,10 +3,35 @@ import glob
 if __name__ == "__main__":
 
     indir = "resultstmp"
-    files = glob.glob(indir+"/*T2tt*.dat")
+    outfilename = "BGnonQCD.dat"
+    files_extension = ".dat"
+
+    things_to_veto = ["QCD"]
+    things_to_keep = []
+
+    files = glob.glob(indir+"/*"+files_extension)
     count = 0
-    with open("T2tt.dat",'w') as outfile:
+    with open(outfilename,'w') as outfile:
         for f in files:
+            stop = False
+            if len(things_to_veto) > 0:
+                for veto in things_to_veto:
+                    if veto in f:
+                        stop = True
+                        break
+            if stop: continue
+
+            stop2 = False
+            if len(things_to_keep) > 0:
+                anything_to_keep = False
+                for keep in things_to_keep:
+                    if keep in f:
+                        anything_to_keep = True
+                        break
+                if not anything_to_keep:
+                    stop2 = True
+            if stop2: continue
+            
             with open(f) as infile:
                 for line in infile:
                     if "MR" in line and count == 1: continue
