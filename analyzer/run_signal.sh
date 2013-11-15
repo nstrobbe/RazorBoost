1ee047b8e5a802871cc861b8d2519d894df9758c
Nadja <nadja.strobbe@cern.ch>
2013-11-15 14:58:37 +0100

    Added file to run over signal

---
+++ analyzer/run_signal.sh
@@ -0,0 +1,8 @@
+#*************************************************
+#*** This file contains the commands to run    ***
+#*** over a single signal point, including the ***
+#*** cross section and total weight            ***
+#*************************************************
+
+./rzrBoostMC filelists/filelist_T1ttcc_325_300.txt 0.0243547 128650 19789 rzrBoostMC_T1ttcc_325_300.root
+
