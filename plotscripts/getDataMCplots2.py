4daed2cada91b3f4d6b0d73a92feb77875ed2f20
Nadja <nadja.strobbe@cern.ch>
2013-11-21 18:44:03 +0100

    added more functionality to plotTools, and new script to make Data/MC plots

---
+++ plotscripts/getDataMCplots2.py
@@ -0,0 +1,82 @@
+import sys, os
+from string import *
+import ROOT as rt
+from ROOT import TFile
+
+import plotTools
+
+if __name__ == '__main__':
+
+    outputdir = "test_plots"
+    inputdir = "/afs/cern.ch/work/n/nstrobbe/RazorBoost/GIT/Results/results_20131120/summary/"
+    analyzer ="rzrBoostMC"
+    
+    if not os.path.isdir(outputdir):
+        os.mkdir(outputdir)
+
+    outfile = TFile.Open(outputdir+"/plots.root","RECREATE")
+
+    # Integrated luminosity in fb-1s
+    intlumi = 19.789 # ABCD
+
+    plotTools.SetBoostStyle()
+    
+    print "Will make plots for integrated luminosity of %.3f fb-1" % (intlumi)
+
+    # define all the datasets we want to plot, and their colors
+    # backgrounds
+    mc_datasets = ["QCD","TTJets","WJetsToLNu","Wbb","Top","TTX","ZJetsToNuNu","DYJetsToLL","DYToBB","DYToCC","VV","VVV"]
+    mc_colors   = [rt.kMagenta,rt.kRed,rt.kGreen+1,rt.kGreen+3,rt.kCyan,rt.kCyan+2,rt.kOrange,rt.kOrange+2,rt.kOrange+7,rt.kOrange+9,rt.kBlue+1,rt.kBlue-3]
+    flist = []
+    for d in mc_datasets:
+        f = TFile.Open(inputdir+analyzer+"_"+d+".root")
+        flist.append(f)
+    # signal
+    sig_datasets = ["T1ttcc_325_300"]
+    sig_colors   = [rt.kGray]
+    fsiglist = []
+    for d in sig_datasets:
+        f = TFile.Open(inputdir+analyzer+"_"+d+".root")
+        fsiglist.append(f)
+    # data
+    fdata = TFile.Open(inputdir+analyzer+"_data.root")
+        
+    # make the dictionaries to pass to the plot routine
+    vars = ["MR","R2"]
+    cuts = ["Cleaning","HCAL_noise","vertexg0","njetge3","HLT","jet1ptg200",
+            "SIG","neleeq0","nmueq0","trackIso",
+            "g1Mb0Ll","g1Mbg1W0Ll","0Lb0Ll","0Lbg1uW0Ll","0Lbg1uW0Ll_mdPhi0p3","0Lbg1W0Ll",
+            "1Ll","g1Mb1Ll","g1Mbg1W1Ll","g1Mbg1W1LlmT100","g1Mbg1W1LlmT",
+            "2munoZmass","2mu","2mu0el","0Lb2mu0el","0Lbg1Y2mu0el","g1Mb2mu0el","g1Mbg1Y2mu0el"
+            ]
+
+    for cut in cuts:
+        for var in vars:
+            hname = "h_%s_%s" % (var,cut)
+            htitle = "Data/MC comparison plot"
+            hlist = []
+            for i in range(len(mc_datasets)):
+                if not flist[i]: continue
+                hdict = plotTools.ConstructHDict(flist[i].Get(hname),name=mc_datasets[i],color=mc_colors[i],title=htitle)
+                hlist.append(hdict)
+        
+            hsiglist = []
+            for i in range(len(sig_datasets)):
+                if not fsiglist[i]: continue
+                hdict = plotTools.ConstructHDict(fsiglist[i].Get(hname),name=sig_datasets[i],color=sig_colors[i],title=htitle)
+                hsiglist.append(hdict)
+
+            hdict_data = plotTools.ConstructHDict(fdata.Get(hname),name="data",color=rt.kBlack,title=htitle,xtitle="MR",ytitle="Events",markerstyle=20)
+
+            # now make the actual plot
+            plotTools.PlotDataMC(hlist,hdict_data,hsiglist,outputdir=outputdir, outfile=outfile,
+                                 cname="DataMC_%s_%s"%(var,cut), plotinfo="Selection %s"%(cut),
+                                 ratiotitle="Data/MC", logscale=True, scale="No")
+
+            htitle = "Data/MC comparison plot with QCD scaled up to match Data in first bin"
+            plotTools.PlotDataMC(hlist,hdict_data,hsiglist,outputdir=outputdir, outfile=outfile,
+                                 cname="DataMC_%s_%s_QCDscaled"%(var,cut), plotinfo="Selection %s"%(cut),
+                                 ratiotitle="Data/MC", logscale=True, scale="QCD")
+
+    outfile.Close()
+
--- plotscripts/plotTools.py
+++ plotscripts/plotTools.py
@@ -5,11 +5,17 @@
 import sys, os
 import ROOT as rt
 
+# ---------------------------------------------- #
+# -- ROOT style                               -- #
+# ---------------------------------------------- # 
 def SetBoostStyle():
     rt.gStyle.SetOptStat(0)
     rt.gStyle.SetTextFont(42)
     rt.TH1.SetDefaultSumw2()
 
+# ---------------------------------------------- #
+# -- Constructor for histogram dictionary     -- #
+# ---------------------------------------------- # 
 def ConstructHDict(h, name="name", color=rt.kBlue, title="title", appear_in_ratio="Yes"
                    , linestyle=1, linewidth=2, markerstyle=7, markersize=1, fillstyle=0
                    , xtitle="", ytitle=""):
@@ -28,6 +34,9 @@ def ConstructHDict(h, name="name", color=rt.kBlue, title="title", appear_in_rati
     hdict["ytitle"] = ytitle                   # y axis title
     return hdict
 
+# ---------------------------------------------- #
+# -- Constructor for legend dictionary        -- #
+# ---------------------------------------------- # 
 def ConstructLDict(xmin,xmax,ymin,ymax,title):
     ldict = {}
     legdict["xmin"] = xmin
@@ -37,15 +46,18 @@ def ConstructLDict(xmin,xmax,ymin,ymax,title):
     legdict["title"] = title
     return ldict
 
+# ---------------------------------------------- #
+# -- Plot routine for 2D plots                -- #
+# ---------------------------------------------- # 
 def Plot2D():
     pass
 
-def Plot1DWithRatio(hdictlist=0,outputdir="plots",outfile=0,legdict=0,cname="canvas"
+# ---------------------------------------------- #
+# -- Plot routine for 1D comparison plots     -- #
+# ---------------------------------------------- # 
+def Plot1DWithRatio(hdictlist,outputdir="plots",outfile=0,legdict=0,cname="canvas"
                     ,ratiotitle="ratio",logscale=False,scale=True):
     # First do some checks on the input
-    if hdictlist == 0:
-        print "You did not give a list of histogram dictionaries, will quit"
-        sys.exit(0)
     if outfile == 0:
         print "You did not pass me a root file to store the plots. I will only produce pdf files."
     if not os.path.isdir(outputdir):
@@ -154,6 +166,178 @@ def Plot1DWithRatio(hdictlist=0,outputdir="plots",outfile=0,legdict=0,cname="can
         firstB = firstB+1
         
     print "Drew the ratio plot"
+
+    rt.SetOwnership(pad1, False) # to avoid seg fault
+    rt.SetOwnership(pad2, False) # to avoid seg fault
+    
+    canvas.cd()
+    canvas.SaveAs(outputdir+"/"+cname+".pdf")
+    if outfile != 0:
+        outfile.cd()
+        canvas.Write()
+    canvas.Close()
+
+# ----------------------------------------------- #
+# -- Plot routine for Data/MC comparison plots -- #
+# ----------------------------------------------- # 
+def PlotDataMC(hdictlist_bg, hdict_data, hdictlist_sig=0, legdict=0
+               , outputdir="plots", outfile=0, cname="canvas", plotinfo="Selection X"
+               , ratiotitle="ratio", logscale=False, scale="No", intlumi=19.789):
+
+    # First do some checks on the input
+    if outfile == 0:
+        print "You did not pass me a root file to store the plots. I will only produce pdf files."
+    if not os.path.isdir(outputdir):
+        print "Output directory doesn't exist yet. \nWill create directory %s" % (outputdir)
+        os.makedirs(outputdir)
+
+    # Get clones of all the mc histograms and put them in a list
+    hQCD_index = -1 # will need this if we want to scale QCD only
+    histos = []
+    for i,hdict in enumerate(hdictlist_bg):
+        # first check that the histogram exists
+        if not hdict["histogram"]:
+            print "Histogram for sample", hdict["name"], "and canvas", cname, "doesn't exist, will stop making this plot now"
+            return
+        h = hdict["histogram"].Clone() 
+        h.Sumw2()
+        h.SetFillColor(hdict["color"])
+        h.SetLineColor(hdict["color"])
+        histos.append(h)
+        if "QCD" in hdict["name"]:
+            hQCD_index = i
+
+    # Get data histogram
+    if not hdict_data["histogram"]:
+        print hdict["name"], "doesn't exist, will stop making this plot now"
+        return
+    hdata = hdict_data["histogram"].Clone()
+    hdata.SetMarkerStyle(hdict_data["markerstyle"])
+    hdata.SetLineColor(hdict_data["color"])
+    hdata.GetYaxis().SetTitle(hdict_data["ytitle"])
+    hdata.SetTitle(hdict_data["title"])
+
+    # Get signal histograms
+    hsignal = []
+    for hdict in hdictlist_sig:
+        if not hdict["histogram"]:
+            print hdict["name"], "doesn't exist, will stop making this plot now"
+            return
+        h = hdict["histogram"].Clone()
+        h.Sumw2()
+        h.SetFillColor(hdict["color"])
+        h.SetLineColor(hdict["color"])
+        hsignal.append(h)
+
+    # make a stack of all the mc, will use the reverse order of the list
+    mc = rt.THStack()
+    for h in reversed(histos):
+        mc.Add(h,"hist")
+    for h in hsignal:
+        mc.Add(h,"hist")
+        
+    # make the total mc histogram, used for the ratio plot
+    htotal = histos[0].Clone()
+    for h in histos[1:]:
+        htotal.Add(h)
+
+    # scale MC to data if required
+    if scale == "Yes":
+        mc_int = htotal.Integral(0,htotal.GetNbinsX()+1)
+        if mc_int == 0:
+            mc_int = 1.
+        data_int = hdata.Integral()
+        sf = data_int/mc_int
+        for h in histos:
+            h.Scale(sf)
+        print "Scaled all histograms by factor", sf
+    # scale only QCD to match data in the first non-empty bin
+    if scale == "QCD":
+        # find first non-empty bin
+        first_bin = 0
+        for b in range(hdata.GetNbinsX()):
+            if hdata.GetBinContent(b+1)>0:
+                first_bin = b+1
+                break
+        # compute the scale factor
+        sf = 1
+        if histos[hQCD_index].GetBinContent(first_bin) != 0:
+            sf = (hdata.GetBinContent(first_bin) - htotal.GetBinContent(first_bin) + histos[hQCD_index].GetBinContent(first_bin)) / histos[hQCD_index].GetBinContent(first_bin)
+        histos[hQCD_index].Scale(sf)
+        print "Scaled QCD histogram by factor", sf
+    if scale != "No":
+        # we scaled some histograms, will need to remake htotal
+        htotal = histos[0].Clone()
+        for h in histos[1:]:
+            htotal.Add(h)
+        
+        
+    # make the legend
+    legend = rt.TLegend(0.63,0.4,0.87,0.8,"")
+    if legdict != 0:
+        legend = rt.TLegend(legdict["xmin"],legdict["ymin"],legdict["xmax"],legdict["ymax"],legdict["title"])
+    legend.SetFillColor(0)
+    legend.SetBorderSize(0)
+    legend.AddEntry(hdata," data ("+ str(intlumi) + "/fb)","epl")
+    for i,h in enumerate(histos):
+        legend.AddEntry(h,hdictlist_bg[i]["name"],"f")
+    for i,h in enumerate(hsignal):
+        legend.AddEntry(h,hdictlist_sig[i]["name"],"f")
+
+    # Get the maximum of the histograms, so that we can set the Y-axis range
+    maxi = hdata.GetMaximum()
+    if htotal.GetMaximum() > maxi:
+        maxi = htotal.GetMaximum()
+
+    # Make the canvas, which will have two pads
+    canvas = rt.TCanvas(cname,"")
+    pad1 = rt.TPad("pad1","",0,0.25,1,1)
+    pad1.SetBottomMargin(0)
+    if logscale:
+        pad1.SetLogy(1)
+    pad1.Draw()
+    pad1.cd()
+
+    hdata.GetYaxis().SetTitleSize(0.055)
+    hdata.GetYaxis().SetTitleOffset(0.8)
+    hdata.GetYaxis().SetLabelSize(0.05)
+    hdata.SetMaximum(2.*maxi)
+    hdata.SetMinimum(0.007)
+    hdata.Draw("EP")
+    mc.Draw("same")
+    hdata.Draw("EPsame")
+    legend.Draw("same")
+    t = '#scale[1.1]{%s}' % (plotinfo)
+    tex = rt.TLatex(0.52,0.82,t)
+    tex.SetNDC();
+    tex.Draw("same");
+
+
+    # Make the second pad, with the ratios
+    canvas.cd()
+    pad2 = rt.TPad("pad2","",0,0,1,0.25)
+    pad2.SetBottomMargin(0.35)#0.25
+    pad2.SetTopMargin(0)#0.05
+    pad2.SetGridy(1)
+    pad2.Draw()
+    pad2.cd()
+
+    ratio = hdata.Clone()
+    ratio.Divide(htotal)
+    ratio.SetTitle("")
+    ratio.SetName("histoRatio")
+    ratio.GetYaxis().SetRangeUser(0,2.2)
+    ratio.GetYaxis().SetNdivisions(4,8,0)
+    ratio.GetYaxis().SetLabelSize(0.14)
+    ratio.GetYaxis().SetTitleSize(0.15)
+    ratio.GetYaxis().SetTitle(ratiotitle)
+    ratio.GetYaxis().SetTitleOffset(0.22)
+    ratio.GetXaxis().SetLabelSize(0.13)
+    ratio.GetXaxis().SetTitleSize(0.15)
+    ratio.GetXaxis().SetTickLength(0.1)
+    ratio.GetXaxis().SetTitle(hdict_data["xtitle"])
+    ratio.SetStats(0)
+    ratio.Draw("pe")
 
     rt.SetOwnership(pad1, False) # to avoid seg fault
     rt.SetOwnership(pad2, False) # to avoid seg fault
