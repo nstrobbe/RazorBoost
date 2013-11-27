ba796af6b7614e43f8e224315486d5d36ab9350e
Nadja <nadja.strobbe@cern.ch>
2013-11-27 19:52:47 +0100

    Added more signals with their information

--- analyzer/run_signal.sh
+++ analyzer/run_signal.sh
@@ -5,4 +5,11 @@
 #*************************************************
 
 ./rzrBoostMC filelists/filelist_T1ttcc_325_300.txt 0.0243547 128650 19789 rzrBoostMC_T1ttcc_325_300.root
+#./rzrBoostMC filelists/filelist_T1ttcc_375_300.txt 0.0243547 129094 19789 rzrBoostMC_T1ttcc_375_300.root
+#./rzrBoostMC filelists/filelist_T1ttcc_510_500.txt 0.0243547 128744 19789 rzrBoostMC_T1ttcc_510_500.root
+#./rzrBoostMC filelists/filelist_T1ttcc_575_500.txt 0.0243547 128754 19789 rzrBoostMC_T1ttcc_575_500.root
 
+#./rzrBoostMC filelists/filelist_T2tt_400_50.txt 0.35683 127868 19789 rzrBoostMC_T2tt_400_50.root
+#./rzrBoostMC filelists/filelist_T2tt_500_100.txt 0.0855847 123636 19789 rzrBoostMC_T2tt_500_100.root
+#./rzrBoostMC filelists/filelist_T2tt_600_50.txt 0.0248009 120636 19789 rzrBoostMC_T2tt_600_50.root
+#./rzrBoostMC filelists/filelist_T2tt_700_1.txt 0.0081141 118665 19789 rzrBoostMC_T2tt_700_1.root
