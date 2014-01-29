f953b616473007de1f1f25a59583eebab0edf8fc
Nadja <nadja.strobbe@cern.ch>
2014-01-29 15:06:00 +0100

    added info on new signals

--- analyzer/run_signal.sh
+++ analyzer/run_signal.sh
@@ -4,7 +4,7 @@
 #*** cross section and total weight            ***
 #*************************************************
 
-./rzrBoostMC filelists/filelist_T1ttcc_325_300.txt 0.0243547 128650 19789 rzrBoostMC_T1ttcc_325_300.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_325_300.txt 0.0243547 128650 19789 rzrBoostMC_T1ttcc_325_300.root T1ttcc ISR_False TopPt_False Pileup_True
 #./rzrBoostMC filelists/filelist_T1ttcc_375_300.txt 0.0243547 129094 19789 rzrBoostMC_T1ttcc_375_300.root T1ttcc ISR_False TopPt_False Pileup_True
 #./rzrBoostMC filelists/filelist_T1ttcc_510_500.txt 0.0243547 128744 19789 rzrBoostMC_T1ttcc_510_500.root T1ttcc ISR_False TopPt_False Pileup_True
 #./rzrBoostMC filelists/filelist_T1ttcc_575_500.txt 0.0243547 128754 19789 rzrBoostMC_T1ttcc_575_500.root T1ttcc ISR_False TopPt_False Pileup_True
@@ -13,3 +13,7 @@
 #./rzrBoostMC filelists/filelist_T2tt_500_100.txt 0.0855847 123636 19789 rzrBoostMC_T2tt_500_100.root T2tt ISR_False TopPt_False Pileup_True
 #./rzrBoostMC filelists/filelist_T2tt_600_50.txt 0.0248009 120636 19789 rzrBoostMC_T2tt_600_50.root T2tt ISR_False TopPt_False Pileup_True
 #./rzrBoostMC filelists/filelist_T2tt_700_1.txt 0.0081141 118665 19789 rzrBoostMC_T2tt_700_1.root T2tt ISR_False TopPt_False Pileup_True
+
+./rzrBoostMC filelists/filelist_T1ttcc_1000_325_300.txt 0.0243547 32195 19789 rzrBoostMC_T1ttcc_1000_325_300.root T1ttcc ISR_False TopPt_False Pileup_True
+./rzrBoostMC filelists/filelist_T1ttcc_1250_75_50.txt 0.00291565 33934 19789 rzrBoostMC_T1ttcc_1250_75_50.root T1ttcc ISR_False TopPt_False Pileup_True
+./rzrBoostMC filelists/filelist_T1ttcc_1250_26_1.txt 0.00291565 33945 19789 rzrBoostMC_T1ttcc_1250_26_1.root T1ttcc ISR_False TopPt_False Pileup_True
