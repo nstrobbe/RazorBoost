e5d364622bd86b31afae767a8095aa5161929a57
Nadja <nadja.strobbe@cern.ch>
2014-02-12 16:51:40 +0100

    Added ISR weight for signal

--- analyzer/run_signal.sh
+++ analyzer/run_signal.sh
@@ -17,3 +17,7 @@
 ./rzrBoostMC filelists/filelist_T1ttcc_1000_325_300.txt 0.0243547 32195 19789 rzrBoostMC_T1ttcc_1000_325_300.root T1ttcc ISR_False TopPt_False Pileup_True
 ./rzrBoostMC filelists/filelist_T1ttcc_1250_75_50.txt 0.00291565 33934 19789 rzrBoostMC_T1ttcc_1250_75_50.root T1ttcc ISR_False TopPt_False Pileup_True
 ./rzrBoostMC filelists/filelist_T1ttcc_1250_26_1.txt 0.00291565 33945 19789 rzrBoostMC_T1ttcc_1250_26_1.root T1ttcc ISR_False TopPt_False Pileup_True
+
+
+# Using ISR weights
+#./rzrBoostMC filelists/filelist_T1ttcc_325_300.txt 0.0243547 123871.1 19789 rzrBoostMC_T1ttcc_325_300.root T1ttcc ISR_True TopPt_False Pileup_True
