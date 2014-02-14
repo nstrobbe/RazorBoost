453e646a4c8dfb731ce5df2e7dcde4105aca5fe9
Nadja <nadja.strobbe@cern.ch>
2014-02-14 15:48:24 +0100

    added filelists for new signal skims

--- analyzer/run_signal.sh
+++ analyzer/run_signal.sh
@@ -14,9 +14,16 @@
 #./rzrBoostMC filelists/filelist_T2tt_600_50.txt 0.0248009 120636 19789 rzrBoostMC_T2tt_600_50.root T2tt ISR_False TopPt_False Pileup_True
 #./rzrBoostMC filelists/filelist_T2tt_700_1.txt 0.0081141 118665 19789 rzrBoostMC_T2tt_700_1.root T2tt ISR_False TopPt_False Pileup_True
 
+#./rzrBoostMC filelists/filelist_T1ttcc_600_26_1.txt 1.31429 29778 19789 rzrBoostMC_T1ttcc_600_26_1.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_600_275_250.txt 1.31429 29065 19789 rzrBoostMC_T1ttcc_600_275_250.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_800_125_100.txt 0.157399 30005 19789 rzrBoostMC_T1ttcc_800_125_100.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_800_375_350.txt 0.157399 30578 19789 rzrBoostMC_T1ttcc_800_375_350.root T1ttcc ISR_False TopPt_False Pileup_True
 ./rzrBoostMC filelists/filelist_T1ttcc_1000_325_300.txt 0.0243547 32195 19789 rzrBoostMC_T1ttcc_1000_325_300.root T1ttcc ISR_False TopPt_False Pileup_True
-./rzrBoostMC filelists/filelist_T1ttcc_1250_75_50.txt 0.00291565 33934 19789 rzrBoostMC_T1ttcc_1250_75_50.root T1ttcc ISR_False TopPt_False Pileup_True
-./rzrBoostMC filelists/filelist_T1ttcc_1250_26_1.txt 0.00291565 33945 19789 rzrBoostMC_T1ttcc_1250_26_1.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_1200_26_1.txt 0.00440078 33463 19789 rzrBoostMC_T1ttcc_1200_26_1.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_1200_125_100.txt 0.00440078 32939 19789 rzrBoostMC_T1ttcc_1200_125_100.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_1250_75_50.txt 0.00291565 33934 19789 rzrBoostMC_T1ttcc_1250_75_50.root T1ttcc ISR_False TopPt_False Pileup_True
+#./rzrBoostMC filelists/filelist_T1ttcc_1250_26_1.txt 0.00291565 33945 19789 rzrBoostMC_T1ttcc_1250_26_1.root T1ttcc ISR_False TopPt_False Pileup_True
+
 
 
 # Using ISR weights
