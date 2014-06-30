033c70ec9ccfdc665e3393e50e939af1ee0d3f81
Nadja <nadja.strobbe@cern.ch>
2014-06-30 11:47:44 +0200

    New systematics files including Wfake SF

--- analyzer/write_syst.py
+++ analyzer/write_syst.py
@@ -4,16 +4,41 @@ import datetime
 
 def generate_line():
     # Here we generate one line in the systematics file
-    # it consists of 14 gaussians, and then a uniform number for the pdfs
+    # it consists of 15 gaussians, and then a uniform number for the pdfs
 
     sysline = ""
-    for i in range(14):
+    for i in range(15):
         number = rd.gauss(0,1)
         sysline = sysline + str(number) + " "
 
     # randrange returns integers
     pdfnumber = rd.randrange(1,301)
     sysline = sysline + str(pdfnumber)
+    return sysline
+
+def generate_lineOnlyPDF():
+    # Here we generate one line in the systematics file
+    # it consists of 15 zeros, and then a uniform number for the pdfs
+
+    sysline = ""
+    for i in range(15):
+        number = 0.
+        sysline = sysline + str(number) + " "
+
+    # randrange returns integers
+    pdfnumber = rd.randrange(1,301)
+    sysline = sysline + str(pdfnumber)
+    return sysline
+
+def generate_lineNoPDF():
+    # Here we generate one line in the systematics file
+    # it consists of 15 gaussians
+
+    sysline = ""
+    for i in range(15):
+        number = rd.gauss(0,1)
+        sysline = sysline + str(number) + " "
+
     return sysline
 
 
